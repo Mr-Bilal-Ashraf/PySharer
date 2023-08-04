a0b3855103a4bd0cb30a100fc65a280319ac2c76
Muhammad Bilal <bilal@synares.com>
2023-08-04 13:05:42 +0500

    flags added and linter apllied

--- setup.py
+++ setup.py
@@ -1,16 +1,20 @@
 import os
 import socket
-
+from argparse import ArgumentParser
 from pathlib import Path
+from platform import system
 
-from flask import Flask, render_template, request, send_from_directory, redirect
+from flask import Flask, redirect, render_template, request, send_from_directory
 from werkzeug.utils import secure_filename
+
 from FILE_TYPES import FILE_TYPES
 
 app = Flask(__name__)
 app.debug = True
 
 port = 5000
+dot_files = False
+OS_SYSTEM = system()
 
 
 def get_file_size(file):
@@ -26,8 +30,7 @@ def determine_file_types(files, path):
         data.append(
             dict(
                 name=file,
-                type=FILE_TYPES.get(
-                    file[file.rfind(".") + 1:].lower(), "unknown"),
+                type=FILE_TYPES.get(file[file.rfind(".") + 1 :].lower(), "unknown"),
                 size=size,
             )
         )
@@ -39,28 +42,32 @@ def determine_file_types(files, path):
 def main(path=""):
     data = dict()
     data["path"] = path
-    path = path.replace("-", "/")
+    path = path.replace("~", "/")
     if not path:
         selected_path = f"{os.path.expanduser('~')}/"
     else:
         selected_path = os.path.expanduser("~") + f"/{path}/"
 
     files = os.listdir(selected_path)
-    data["dirs"] = [
-        f for f in files if os.path.isdir(selected_path + f) and not f.startswith(".")
-    ]
-    data["files"] = [
-        f for f in files if os.path.isfile(selected_path + f) and not f.startswith(".")
-    ]
+
+    list_dot_files = "" if dot_files else " and not f.startswith('.')"
+    find_dir = (
+        f"[f for f in files if os.path.isdir('{selected_path}' + f){list_dot_files}]"
+    )
+    find_files = (
+        f"[f for f in files if os.path.isfile('{selected_path}' + f){list_dot_files}]"
+    )
+
+    data["dirs"] = eval(find_dir)
+    data["files"] = eval(find_files)
     data["dirs"].sort()
     data["files"].sort()
     data["files"] = determine_file_types(data["files"], selected_path)
     data["prev_dir"] = (
-        str(Path(f"/{path}").parent.absolute()
-            ).replace("/", "-")[1:] if path else None
+        str(Path(f"/{path}").parent.absolute()).replace("/", "~")[1:] if path else None
     )
     data["full_path"] = selected_path
-    data["append_slash"] = "-" if path else ""
+    data["append_slash"] = "~" if path else ""
     data["localhost"] = socket.gethostbyname(socket.gethostname())
     data["port"] = port
 
@@ -69,7 +76,7 @@ def main(path=""):
 
 @app.route("/download/<path>/<file>/")
 def download_file(path="", file=""):
-    path = path.replace("-", "/")
+    path = path.replace("~", "/")
     path = os.path.expanduser("~") + f"/{path}/"
     return send_from_directory(path, file, as_attachment=True)
 
@@ -84,4 +91,11 @@ def upload_file():
 
 
 if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("--port")
+    parser.add_argument("--dot_files")
+    args = parser.parse_args()
+    port = args.port if args.port else 5000
+    dot_files = True if args.dot_files and args.dot_files != "0" else False
+
     app.run(host="0.0.0.0", port=port)
